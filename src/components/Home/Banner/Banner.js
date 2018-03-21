31a2f42525d945861d427ed015b2d3f582b6fdb7
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-03-21 17:55:59 -0300

    añadiendo estilos al banner

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -5,8 +5,8 @@ import { Button } from 'mdbreact';
 const Banner = () => {
   return (
     <div id="banner">
-      <h2>What do you want to do today?</h2>
-      <Button color="warning">Warning</Button>
+      <h2>Come Join Us!</h2>
+      <Button color="warning">Register</Button>
     </div>
   );
 };
