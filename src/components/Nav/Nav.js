0662e81206f089536cf27d66cda116bae1e58e6a
mickequeen <mickequeen@gmail.com>
2018-03-21 12:51:06 -0300

    cambio de nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,34 +1,52 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import './Nav.css';
 class Nav extends Component{
   render(){
     return(
       <header>
-        <nav className="mb-1 navbar navbar-expand-lg navbar-dark cyan">
-          <Link className="navbar-brand" to="/">Navbar</Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4" aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
+        <nav className="mb-1 navbar navbar-expand-lg navbar-dark purple lighten-1">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-5" aria-controls="navbarSupportedContent-5" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent-4">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item active">
-                <a className="nav-link waves-effect waves-light" href="#">
-                <i className="fa fa-envelope"></i> Contact <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link waves-effect waves-light" href="#"><i className="fa fa-gear"></i> Settings</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle waves-effect waves-light" id="navbarDropdownMenuLink-4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i className="fa fa-user"></i> Profile </a>
-                <div className="dropdown-menu dropdown-menu-right dropdown-cyan" aria-labelledby="navbarDropdownMenuLink-4">
-                    <a className="dropdown-item waves-effect waves-light" href="#">My account</a>
-                    <a className="dropdown-item waves-effect waves-light" href="#">Log out</a>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </nav>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent-5">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <a className="nav-link waves-effect waves-light" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link waves-effect waves-light" href="#">Features</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link waves-effect waves-light" href="#">Pricing</a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle waves-effect waves-light" id="navbarDropdownMenuLink-5" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown 
+                  </a>
+                  <div className="dropdown-menu dropdown-purple" aria-labelledby="navbarDropdownMenuLink-5">
+                    <a className="dropdown-item waves-effect waves-light" href="#">Action</a>
+                    <a className="dropdown-item waves-effect waves-light" href="#">Another action</a>
+                    <a className="dropdown-item waves-effect waves-light" href="#">Something else here</a>
+                  </div>
+                </li>
+              </ul>
+              <ul className="navbar-nav ml-auto nav-flex-icons">
+                <li className="nav-item">
+                  <a className="nav-link waves-effect waves-light">1 <i className="fa fa-envelope"></i></a>
+                </li>
+                <li class="nav-item avatar dropdown show">
+                  <a className="nav-link dropdown-toggle waves-effect waves-light" id="navbarDropdownMenuLink-5" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-2.jpg" class="img-fluid rounded-circle z-depth-0" alt="avatar image"/></a>
+                  <div className="dropdown-menu dropdown-menu-right dropdown-purple" aria-labelledby="navbarDropdownMenuLink-5">
+                    <a className="dropdown-item waves-effect waves-light" href="#">Action</a>
+                    <a className="dropdown-item waves-effect waves-light" href="#">Another action</a>
+                    <a className="dropdown-item waves-effect waves-light" href="#">Something else here</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </nav>
       </header>
     )
   }
