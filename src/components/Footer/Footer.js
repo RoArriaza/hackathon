4048123ccd1b192ce46fb93a2686d060ea9034a6
mickequeen <mickequeen@gmail.com>
2018-03-21 22:50:29 -0300

    styles footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,29 +1,29 @@
 
 import React from 'react';
 import { Col, Container, Row, Footer } from 'mdbreact';
+import'./Footer.css';
 
 class FooterPage extends React.Component {
-    render(){
-        return(
-            <Footer color="warning-color-dark" className="font-small pt-0">
-                <Container>
-                    <Row>
-                        <Col md="12">
-                            <div className="mb-5 flex-center">
-                            <a className="fb-ic"><i className="fa fa-arrow-left mr-md-5 mr-3 fa-2x"></i><span>Go Back</span></a>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-                <div className="footer-copyright text-center">
-                    <Container fluid>
-                        &copy; {(new Date().getFullYear())} Copyright: <a href="#"> LaboratoriaSCL </a>
-                    </Container>
-                </div>
-            </Footer>
-        );
-    }
+  render(){
+    return(
+      <Footer color="warning-color-dark" className="font-small pt-0">
+        <Container>
+          <Row>
+            <Col md="12">
+              <div className="mb-5 flex-center">
+              <a className="arrowIcon"><i className="fa fa-arrow-left mr-md-5 mr-3 fa-2x"></i><span>Go Back</span></a>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+        <div className="footer-copyright text-center">
+          <Container fluid>
+            &copy; {(new Date().getFullYear())} Copyright: <a href="#"> LaboratoriaSCL </a>
+          </Container>
+        </div>
+      </Footer>
+    );
+  }
 }
-        
+
 export default FooterPage;
-                      
