e0b32fba97f85f216360207740cf9d3b8edc53cc
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-03-21 21:52:53 -0300

    verificando usuario conectado en CreateEvent

--- src/components/CreateEvent/CreateEvent.js
+++ src/components/CreateEvent/CreateEvent.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import { Input } from 'mdbreact';
+import firebase from 'firebase';
 
 class CreateEvent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    };
+  }
+
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user });
+      console.log(this.state.user.email);
+    })
+  }
   render() {
     return (
+      this.state.user !== null ?
+      <div id="create-event">
+        <h2 className="mb-5">Create a new event</h2>
+        <form>
+          <p className="h5 text-center mb-4">Write to us</p>
+          <Input label="Your name" icon="user" group type="email" validate error="wrong" success="right"/>
+        </form>
+      </div>
+      : 
       <div>
-        <p>Create Event</p>
+        <p>Register first :v</p>
       </div>
     );
   }
