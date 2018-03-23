8819064e81df840c8eab5a9aca62ec28cae8a3e6
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-03-23 00:47:45 -0300

    obteniendo eventos desde firebase

--- src/components/CardEvent/CardEvent.js
+++ src/components/CardEvent/CardEvent.js
@@ -5,53 +5,73 @@ import './CardEvent.css';
 import Rebase from 're-base';
 import firebase from 'firebase';
 
-class CardEvent extends Component{
+const numbers = [1,2,3,4];
+
+class CardEvent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: null
+    }
+  }
   showData() {
     var base = Rebase.createClass(firebase.database());
     base.fetch('users', {
       context: this,
       asArray: true
     }).then(data => {
-      console.log(data);
+      let results = [];
+      let addToState = [];
+      data.map((item) => {
+        results.push(item.created);
+      })
+      results.map((item) => {
+        const vals = Object.keys(item).map(key => item[key]);
+        addToState.push(vals);
+      })
+      this.setState({
+        data: addToState
+      });
+    })
+    .then(() => {
+      console.log(this.state);
     })
   }
   componentDidMount() {
     this.showData();
   }
-  render = () => {
-    let superReturn = [];
-    for (let i = 1; i <= 16; i++) {
-      let url = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + i + '.png';
-      superReturn.push(this.showCard(url, i));
-    }
-    return (superReturn);
-}
-
-  showCard(url) {
+  render() {
     return (
       <div className="cardContainer">
-        <Card>
-          <CardImage className="img-fluid mx-auto" src={url} />
-          <CardBody>
-            <CardTitle>Evento Random</CardTitle>
-            <CardText>
-              Descripcion del Evento "holiholiratónconcoli"
-            </CardText>
-        </CardBody>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">Capacidad <i className="fa fa-users" aria-hidden="true"></i></li>
-          <li className="list-group-item">Horario <i className="fa fa-clock-o" aria-hidden="true"></i></li>
-          <li className="list-group-item">Lugar <i className="fa fa-map-marker" aria-hidden="true"></i></li>
-        </ul>
-      
-      <CardBody>
-      <Link className="waves-effect waves-light" to="/"><Button color="warning" href="#">Join!</Button></Link><Link className="waves-effect waves-light" to="event-detail"><Button color="warning" href="#">Detail</Button></Link>
-
-      </CardBody>
-      </Card>
-    </div>
-    )
+      {
+        this.state.data !== null ?
+        this.state.data.map(item => {
+          return item.map(subItem => {
+            return (
+              <Card key={subItem.eventTitle}>
+                <CardImage className="img-fluid mx-auto" src={subItem.eventPhoto} />
+                <CardBody>
+                  <CardTitle>
+                    {subItem.eventTitle}
+                  </CardTitle>
+                  <CardText>
+                    {subItem.eventDescription.substring(0, 150) + '...'}
+                  </CardText>
+                </CardBody>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">Spots {subItem.eventSpots} <i className="fa fa-users" aria-hidden="true"></i></li>
+                  <li className="list-group-item">Time {subItem.eventTime} <i className="fa fa-clock-o" aria-hidden="true"></i></li>
+                  <li className="list-group-item">Place {subItem.eventLocation} <i className="fa fa-map-marker" aria-hidden="true"></i></li>
+                </ul>
+              </Card>
+            )
+          })
+        })
+        : <p>No hay ná</p>
+      }
+      </div>
+    );
   }
-};
+}
 
 export default CardEvent;
