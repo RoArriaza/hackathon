9cb12acae9eacf423e8ef93f7719a004a05168cd
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-03-23 07:57:45 -0300

    mostrando los detalles de cada evento

--- src/components/EventDetail/EventDetail.js
+++ src/components/EventDetail/EventDetail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Input } from 'mdbreact';
 import firebase from 'firebase';
+import Rebase from 're-base';
 import { Button, Card, CardBody, CardImage, CardTitle, CardText } from 'mdbreact';
 import addEvent from './../firebase/AddEvent';
 import './EventDetail.css';
@@ -9,53 +10,77 @@ class EventDetail extends Component {
   constructor() {
     super();
     this.state = {
-      user: null
+      data: null
     };
   }
-
-  componentWillMount() {
-    firebase.auth().onAuthStateChanged(user => {
-      this.setState({ user });
-      // console.log(this.state.user.email);
+  showData() {
+    var base = Rebase.createClass(firebase.database());
+    base.fetch('users', {
+      context: this,
+      asArray: true
+    }).then(data => {
+      let results = [];
+      let addToState = [];
+      data.map((item) => {
+        results.push(item.created);
+      })
+      results.map((item) => {
+        const vals = Object.keys(item).map(key => item[key]);
+        addToState.push(vals);
+      })
+      console.log(addToState);
+      addToState.map(item => {
+        item.map(subItem => {
+          let str = this.props.location.pathname;
+          let preChar = str.replace('/event-detail/', '');
+          if (subItem.eventTitle.replace(/ /g, '-') === preChar) {
+            this.setState({
+              data: subItem
+            })
+          }
+        })
+      })
     })
+    .then(() => {
+      console.log(this.state);
+    })
+  }
+  componentDidMount() {
+    this.showData();
   }
   render() {
     return (
-      /*this.state.user !== null ?*/
       <div id="eventDetail mx-auto">
-        <Card className='detailCard'>
-          <CardImage className="img-fluid mx-auto" src="https://definicion.mx/wp-content/uploads/2014/07/Evento.jpg" />
-          <CardBody>
-            <CardTitle>Evento Random</CardTitle>
-            
-        </CardBody>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item d-flex justify-content-around">
-          <span>Fecha <i className="fa fa-calendar" aria-hidden="true"></i>:</span>
-          <span>Horario <i className="fa fa-clock-o" aria-hidden="true"></i>:</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-around">
-          <span>Capacidad <i className="fa fa-users" aria-hidden="true"></i> :</span>
-          <span>Lugar <i className="fa fa-map-marker" aria-hidden="true"></i>:</span>
-          </li>
-        </ul>
-          <CardText>
-              Descripcion del Evento "holiholiratónconcoli"
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-          </CardText>
+        {
+        this.state.data !== null ?
+          <Card className='detailCard'>
+            <CardImage className="img-fluid mx-auto" src={this.state.data.eventPhoto} />
+            <CardBody>
+              <CardTitle>{this.state.data.eventTitle}</CardTitle>
+              
+          </CardBody>
           <ul className="list-group list-group-flush">
-          <li className="list-group-item text-muted">Creado por:</li>
-          <li className="list-group-item d-flex justify-content-between"><Button color="warning" target="_blank" href="https://twitter.com/home?status=He%20confirmado%20asistencia%20en%20%23JoinUs%20%23JoinMe%20%23Event%20%23Joy">Share<i className="fa fa-twitter"></i></Button><Button color="warning" href="#">Anótame</Button></li>
+            <li className="list-group-item d-flex justify-content-around">
+              <span>Date <i className="fa fa-calendar" aria-hidden="true"></i>: {this.state.data.eventDate}</span>
+              <span>Time <i className="fa fa-clock-o" aria-hidden="true"></i>: {this.state.data.eventTime}</span>
+            </li>
+            <li className="list-group-item d-flex justify-content-around">
+              <span>Spots <i className="fa fa-users" aria-hidden="true"></i>: {this.state.data.eventSpots}</span>
+              <span>Place <i className="fa fa-map-marker" aria-hidden="true"></i>: {this.state.data.eventLocation}</span>
+            </li>
+          </ul>
+            <CardText>
+              {this.state.data.eventDescription}
+            </CardText>
+            <ul className="list-group list-group-flush">
+            <li className="list-group-item text-muted">Created by: {this.state.data.eventOwner}</li>
+            <li className="list-group-item d-flex justify-content-between"><Button color="warning" target="_blank" href="https://twitter.com/home?status=He%20confirmado%20asistencia%20en%20%23JoinUs%20%23JoinMe%20%23Event%20%23Joy">Share<i className="fa fa-twitter"></i></Button><Button color="warning" href="#">Anótame</Button></li>
 
-        </ul>
-        </Card>
+          </ul>
+          </Card>
+          : <p>loading...</p>
+          }
       </div>
-      /*
-      :
-      <div>
-        <p>Register first :v</p>
-      </div>*/
     );
   }
 }
