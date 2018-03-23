93f96d46c2c20c4c84cb1374c532b1238cc95aee
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-03-23 08:44:08 -0300

    actualizando eventSpots

--- src/components/CardEvent/CardEvent.js
+++ src/components/CardEvent/CardEvent.js
@@ -11,15 +11,18 @@ class CardEvent extends Component {
   constructor() {
     super();
     this.state = {
-      data: null
+      data: null,
+      fullData: null
     }
+    this.joinEvent = this.joinEvent.bind(this);
   }
   showData() {
     var base = Rebase.createClass(firebase.database());
     base.fetch('users', {
       context: this,
       asArray: true
-    }).then(data => {
+    })
+    .then(data => {
       let results = [];
       let addToState = [];
       data.map((item) => {
@@ -33,6 +36,42 @@ class CardEvent extends Component {
         data: addToState
       });
     })
+    base.fetch('users', {
+      context: this,
+      asArray: false
+    }).then(data => {
+      this.setState({
+        fullData: data
+      })
+    })
+  }
+  joinEvent(event) {
+    let str = event.target.dataset.title;
+    this.state.data.map(item => {
+      item.map(subItem => {
+        if (subItem.eventTitle === str) {
+          let spots = Number(subItem.eventSpots);
+          spots --;
+          subItem.eventSpots = spots;
+        }
+      })
+    })
+    // Object.keys(obj).map(key => {
+    //   Object.keys(obj[key].created).map(subKey => {
+    //     if (obj[key].created[subKey].eventTitle === str) {
+    //       console.log(obj[key].created[subKey]);
+    //       let spots = Number(obj[key].created[subKey].eventSpots);
+    //       spots --;
+    //       this.state.fullData[key].created[subKey].eventSpots = spots;
+    //     }
+    //   });
+    // });
+    // var base = Rebase.createClass(firebase.database());
+    // base.syncState('users', {
+    //   context: this,
+    //   state: 'fullData',
+    //   asArray: false
+    // })
   }
   componentDidMount() {
     this.showData();
@@ -64,7 +103,7 @@ class CardEvent extends Component {
                     <li className="list-group-item">Place {subItem.eventLocation} <i className="fa fa-map-marker" aria-hidden="true"></i></li>
                   </ul>
                   <CardBody>
-                    <Button color="warning"><Link className="waves-effect waves-light" to="/">Join!</Link></Button>
+                    <Button color="warning" onClick={this.joinEvent} data-title={subItem.eventTitle}>Join!</Button>
                     <Button color="warning"><Link className="waves-effect waves-light" to={`event-detail/${subItem.eventTitle.replace(/ /g, '-')}`} data-title={subItem.eventTitle}>Detail</Link></Button>
                   </CardBody>
                 </Card>
